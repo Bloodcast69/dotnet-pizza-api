ec5699ab8e1aa8175d247c96c29fbb645b48c62f
Damian Gemza <gemza.damian@gmail.com>
2022-06-23 07:49:45 +0200

    Add validation to models

--- Models/User.cs
+++ Models/User.cs
@@ -1,9 +1,13 @@
-﻿namespace ContosoPizza.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace ContosoPizza.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Email address is required")]
+        [EmailAddress]
         public string ? Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
         public string ? Password { get; set; }
         public bool ? Active { get; set; }
     }
--- Models/UserRegisterDTO.cs
+++ Models/UserRegisterDTO.cs
@@ -1,12 +1,20 @@
-﻿namespace ContosoPizza.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContosoPizza.Models
 {
     public class UserRegisterDTO
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "First name is required")]
         public string ? FirstName { get; set; }
+        [Required(ErrorMessage = "Last name is required")]
         public string ? LastName { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
         public string ? Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
         public string ? Password { get; set; }
+        [Required(ErrorMessage = "Confirm password is required")]
         public string ? ConfirmPassword { get; set; }
     }
 }
